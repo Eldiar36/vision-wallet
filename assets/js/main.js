a47d507b3acc081353d0c9794ac62d6c57c9b883
unknown <eldiar36>
2020-08-07 19:05:51 +0600

    edit1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find(".burger-menu_button");
+    let links = menu.find("burger-menu_link");
+    let overlay = menu.find("burger-menu_overlay");
+    button.on("click",(e) => {
+        e.preventDefault();
+        toogleMenu();
+    });
+    links.on("click", () => toogleMenu());
+    overlay.on("click", () => toogleMenu());
+    function toogleMenu() {
+        menu.toggleClass('burger-menu_active');
 
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css("overflow","hidden");
+        }else  {
+            $("body").css("overflow","visible")
+        }
+    }
+}
+burgerMenu('.burger-menu');
 $(document).ready(function() {
     $('.popup-youtube').magnificPopup({
         type: 'iframe',
